3d74d61d24987ee36256157e725169b3ecbcc16a
mygospel <mygospel7@gmail.com>
2022-06-16 07:49:00 +0900

    줄바꿈

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -186,6 +186,8 @@ class EventController extends Controller
 
                 if( !$ev->e_cont2 ) $ev->e_cont2 = $ev->e_cont;
 
+                $ev->e_cont2 = nl2br($ev->e_cont2);
+
                 $data['events'][substr($ev->e_sdate,0,10)][] = $ev;
             }  
 
